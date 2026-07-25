0ab5020b83562b7366a3f8f7b17c3645bd26dc63
Wip-Sama <47224042+Wip-Sama@users.noreply.github.com>
2026-07-25 14:18:11 +0200

    migrates tests to distinct folder

--- test/test_runner.py
+++ test/test_runner.py
@@ -1,11 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 import subprocess
-import os
 
 class TestRunner(unittest.TestCase):
     def test_help_argument_parsing(self):
+        root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        runner_path = os.path.join(root_dir, 'runner.py')
         result = subprocess.run(
-            ['python', 'runner.py', '--help'],
+            [sys.executable, runner_path, '--help'],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
@@ -17,8 +22,10 @@ class TestRunner(unittest.TestCase):
         self.assertEqual(result.returncode, 0)
 
     def test_invalid_opt_argument(self):
+        root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        runner_path = os.path.join(root_dir, 'runner.py')
         result = subprocess.run(
-            ['python', 'runner.py', '--opt', 'invalid_opt'],
+            [sys.executable, runner_path, '--opt', 'invalid_opt'],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
